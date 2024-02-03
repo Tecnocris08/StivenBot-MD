0b45b85bbbce4aec067e1f0c5a7c8a46de812cbd
Support-Bots-OFC <155838423+Bots-WhatsApp-OFC@users.noreply.github.com>
2024-02-02 23:41:09 -0300

    Add files via upload

---
+++ plugins/gc-link.js
@@ -0,0 +1,16 @@
+import fs from 'fs';
+const handler = async (m, {conn, args}) => {
+  const group = m.chat;
+  conn.reply(m.chat, 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), m, {
+    contextInfo: {externalAdReply: {mediaUrl: null, mediaType: 1, description: null,
+      title: '𝙻𝙸𝙽𝙺 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾',
+      body: '❥ᰰຼ ⃟ᬽ៸𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿༒★»',
+      previewType: 0, thumbnail: fs.readFileSync('./Menu2.jpg'),
+      sourceUrl: `https://atom.bio/sakuralite`}}});
+};
+handler.help = ['linkgroup'];
+handler.tags = ['group'];
+handler.command = /^link(gro?up)?$/i;
+handler.group = true;
+handler.botAdmin = true;
+export default handler;
