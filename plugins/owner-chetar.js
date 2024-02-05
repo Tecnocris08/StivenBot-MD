aa623a3c38e2568f5bc05b3ee6f0687396e1ec70
Support-Bots-OFC <155838423+Bots-WhatsApp-OFC@users.noreply.github.com>
2024-02-05 05:07:49 -0300

    Update owner-chetar.js

--- plugins/owner-chetar.js
+++ plugins/owner-chetar.js
@@ -1,6 +1,6 @@
 const handler = async (m, { conn }) => {
     const user = global.db.data.users[m.sender];
-        conn.sendMessage(m.chat, {text: `*🪁✨️ @${m.sender.split('@')[0]} Difruta Tus Recursos Ahora Son Ilimitados.*`, mentions: [m.sender]}, {quoted: m});
+        conn.sendMessage(m.chat, {text: `👑 𝙃𝙤𝙡𝙖 𝙤𝙬𝙣𝙚𝙧 @${m.sender.split('@')[0]}, 𝙩𝙪𝙨 𝙧𝙚𝙘𝙪𝙧𝙨𝙤𝙨 𝙖𝙝𝙤𝙧𝙖 𝙨𝙤𝙣 𝙞𝙡𝙞𝙢𝙞𝙩𝙖𝙙𝙤𝙨. 𝘾𝙝𝙚𝙘𝙖𝙡𝙤 𝙚𝙣 𝙚𝙡 𝙢𝙚𝙣𝙪.`, mentions: [m.sender]}, {quoted: m});
       global.db.data.users[m.sender].money = Infinity;
     global.db.data.users[m.sender].limit = Infinity;
   global.db.data.users[m.sender].level = Infinity;
