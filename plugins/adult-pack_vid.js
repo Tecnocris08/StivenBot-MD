7bb17452ec3d5e794acf9d580335040ad4cc0257
Support-Bots-OFC <155838423+Bots-WhatsApp-OFC@users.noreply.github.com>
2024-02-05 05:03:34 -0300

    Add files via upload

---
+++ plugins/adult-pack_vid.js
@@ -0,0 +1,219 @@
+import fetch from 'node-fetch';
+const handler = async (m, {conn, command, usedPrefix}) => {
+  if (!db.data.chats[m.chat].modohorny && m.isGroup) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙻𝙾𝚂 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 +𝟷𝟾 𝙴𝚂𝚃𝙰𝙽 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝙳𝙾𝚂 𝙴𝙽 𝙴𝚂𝚃𝙴 𝙶𝚁𝚄𝙿𝙾, 𝚂𝙸 𝙴𝚂 𝙰𝙳𝙼𝙸𝙽 𝚈 𝙳𝙴𝚂𝙴𝙰 𝙰𝙲𝚃𝙸𝚅𝙰𝚁𝙻𝙾𝚂 𝚄𝚂𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 #enable modohorny*';
+  switch (command) {
+    case 'pack':
+      const url = await pack[Math.floor(Math.random() * pack.length)];
+      conn.sendMessage(m.chat, {image: {url: url}, caption: `_🥵 Pack 🥵_`}, {quoted: m});
+      break;
+    case 'pack2':
+      const url2 = await packgirl[Math.floor(Math.random() * packgirl.length)];
+      conn.sendMessage(m.chat, {image: {url: url2}, caption: `_🥵 Pack 🥵_`}, {quoted: m});
+      break;
+    case 'pack3':
+      const url3 = await packmen[Math.floor(Math.random() * packmen.length)];
+      conn.sendMessage(m.chat, {image: {url: url3}, caption: `_🥵 Pack 3 🥵_`}, {quoted: m});
+      break;
+        case 'sexboy3d': case 'xmenfantasy':
+      const url4 = await packmen3d[Math.floor(Math.random() * packmen.length)];
+      conn.sendMessage(m.chat, {image: {url: url3}, caption: `_🥵 Pack men 3D 🥵_`}, {quoted: m});
+      break;
+    case 'videoxxx': case 'vídeoxxx':
+      const url5 = await videosxxxc[Math.floor(Math.random() * videosxxxc.length)];
+      await conn.sendMessage(m.chat, {video: {url: url4}, caption: `*ᴅɪsғʀᴜᴛᴀ ᴅᴇʟ ᴠɪᴅᴇᴏ 🥵*`}, {quoted: m});
+      break;
+    case 'videoxxxlesbi': case 'videolesbixxx': case 'pornolesbivid': case 'pornolesbianavid': case 'pornolesbiv': case 'pornolesbianav': case 'pornolesv':
+      const url6 = await videosxxxc2[Math.floor(Math.random() * videosxxxc2.length)];
+      await conn.sendMessage(m.chat, {video: {url: url5}, caption: `*ᴅɪsғʀᴜᴛᴀ ᴅᴇʟ ᴠɪᴅᴇᴏ 🥵*`}, {quoted: m});
+      break;
+  }
+};
+handler.command = /^(pack|pack2|pack3|sexbot3d|xmenfantasy|videoxxx|vídeoxxx|videoxxxlesbi|videolesbixxx|pornolesbivid|pornolesbianavid|pornolesbiv|pornolesbianav|pornolesv)$/i;
+export default handler;
+
+global.pack = [
+  'https://telegra.ph/file/957fe4031132ef90b66ec.jpg',
+  'https://telegra.ph/file/c4b85bd53030cb648382f.jpg',
+  'https://telegra.ph/file/df56f8a76145df9c923ad.jpg',
+  'https://telegra.ph/file/d5d1c2c710c4b5ee8bc6c.jpg',
+  'https://telegra.ph/file/d0c0cd47e87535373ab68.jpg',
+  'https://telegra.ph/file/651a5a9dc96c97c8ef8fc.jpg',
+  'https://telegra.ph/file/f857ae461ceab18c38de2.jpg',
+  'https://telegra.ph/file/5d2a2aeff5e6fbd229eff.jpg',
+  'https://telegra.ph/file/b93573531f898ea875dd0.jpg',
+  'https://telegra.ph/file/c798b3959f84d345b0f25.jpg',
+  'https://telegra.ph/file/de820647f8cabce533557.jpg',
+  'https://telegra.ph/file/e105097d5fadf3e522eb5.jpg',
+  'https://telegra.ph/file/8592e352a9ee6c7244737.jpg',
+  'https://telegra.ph/file/bb9c7d879b7dc1d86a2ce.jpg',
+  'https://telegra.ph/file/83f108601e6105446ad1f.jpg',
+  'https://telegra.ph/file/2a6bff14e53ed2533ad25.jpg',
+  'https://telegra.ph/file/e37d74aeccc5bdfd6be3e.jpg',
+  'https://telegra.ph/file/ca984650af06b951e961d.jpg',
+  'https://telegra.ph/file/ebb3ac7f7498dd09f6afc.jpg',
+  'https://telegra.ph/file/6192305a24ffb8fa30942.jpg',
+  'https://telegra.ph/file/ee67c17d0043b98dc757e.jpg',
+  'https://telegra.ph/file/6ae756b686cd2b5950721.jpg',
+  'https://telegra.ph/file/b1e1da38d897d117c2aa9.jpg',
+  'https://telegra.ph/file/6b759437dc8b863c2fa19.jpg',
+  'https://telegra.ph/file/960d8c268aecb5eb117f0.jpg',
+  'https://telegra.ph/file/d0dd518bdd147cb10b0b5.jpg',
+  'https://telegra.ph/file/31f2d59b5cd68ec5acb21.jpg',
+  'https://telegra.ph/file/14ab9bd02f24e0f1a1a03.jpg',
+  'https://telegra.ph/file/e02bf6bc9227f7f8b7e2a.jpg',
+  'https://telegra.ph/file/ab55fca1d6b602b1a69df.jpg',
+  'https://telegra.ph/file/42105cac3666b37da3d1c.jpg',
+];
+global.packgirl = [
+  'https://telegra.ph/file/c0da7289bee2d97048feb.jpg',
+  'https://telegra.ph/file/b8564166f9cac4d843db3.jpg',
+  'https://telegra.ph/file/fdefd621a17712be15e0e.jpg',
+  'https://telegra.ph/file/6e1a6dcf1c91bf62d3945.jpg',
+  'https://telegra.ph/file/0224c1ecf6b676dda3ac0.jpg',
+  'https://telegra.ph/file/b71b8f04772f1b30355f1.jpg',
+  'https://telegra.ph/file/6561840400444d2d27d0c.jpg',
+  'https://telegra.ph/file/37e445df144e1dfcdb744.jpg',
+  'https://telegra.ph/file/155b6ac6757bdd9cd05f9.jpg',
+  'https://telegra.ph/file/2255a8a013540c2820a2b.jpg',
+  'https://telegra.ph/file/450e901ac153765f095c5.jpg',
+  'https://telegra.ph/file/f18e421a70810015be505.jpg',
+  'https://telegra.ph/file/d3d190691ec399431434e.jpg',
+  'https://telegra.ph/file/1fd2b897a1dbc3fdc2a70.jpg',
+  'https://telegra.ph/file/607d54a909035bca7444f.jpg',
+  'https://telegra.ph/file/818ba7c0ae82876b190b6.jpg',
+  'https://telegra.ph/file/0f2bb426951b4a8fe1e5a.jpg',
+  'https://telegra.ph/file/7e895b5b933226a07558a.jpg',
+  'https://telegra.ph/file/f9d9f0da337512a1b1882.jpg',
+  'https://telegra.ph/file/09ff5bfce02f1f78e3861.jpg',
+  'https://telegra.ph/file/4ad840d401ab1f90444df.jpg',
+  'https://telegra.ph/file/7b4bdcad3dde870355c94.jpg',
+  'https://telegra.ph/file/f69a5beaca50fc52a4a71.jpg',
+  'https://telegra.ph/file/411d7cdee24669e167adb.jpg',
+  'https://telegra.ph/file/36a63288e27e88e2f8e10.jpg',
+  'https://telegra.ph/file/1ac7657a5e7b354cd9991.jpg',
+  'https://telegra.ph/file/14161eab0c1d919dc3218.jpg',
+  'https://telegra.ph/file/810411b9128fe11dd639a.jpg',
+  'https://telegra.ph/file/5fe7e98533754b007e7a1.jpg',
+];
+global.packmen = [
+  'https://telegra.ph/file/bf303b19b9834f90e9617.jpg',
+  'https://telegra.ph/file/36ef2b807251dfccd17c2.jpg',
+  'https://telegra.ph/file/bcc34403d16de829ea5d2.jpg',
+  'https://telegra.ph/file/5c6b7615662fb53a39e53.jpg',
+  'https://telegra.ph/file/1a8183eff48671ea265c2.jpg',
+  'https://telegra.ph/file/f9745dcd22f67cbc62e08.jpg',
+  'https://telegra.ph/file/02219f503317b0596e101.jpg',
+  'https://telegra.ph/file/470c8ec30400a73d03207.jpg',
+  'https://telegra.ph/file/c94fa8ed20f2c0cf16786.jpg',
+  'https://telegra.ph/file/1b02a1ca6a39e741faec7.jpg',
+  'https://telegra.ph/file/eea58bf7043fd697cdb43.jpg',
+  'https://telegra.ph/file/ee3db7facdfe73c8df05a.jpg',
+  'https://telegra.ph/file/d45b4e4af4f2139507f8c.jpg',
+  'https://telegra.ph/file/d176e7fc8720f98f6b182.jpg',
+  'https://telegra.ph/file/ce1e072829d1fa5d99f5f.jpg',
+  'https://telegra.ph/file/a947933701be6d579c958.jpg',
+  'https://telegra.ph/file/9027e5a464ec88e8ab5c1.jpg',
+  'https://telegra.ph/file/049a8c611a838ea2f6daa.jpg',
+  'https://telegra.ph/file/37b35fbc7e2ee73482ee1.jpg',
+  'https://telegra.ph/file/9bcfade24ae85cd417f06.jpg',
+  'https://telegra.ph/file/ac0c711585f4300c54355.jpg',
+];
+global.packmen3d = [
+  "https://twitter.com/MansterpieceAI/status/1717019999674458225?t=3uYtme8KZ84Ylye5FIBcIA&s=19",
+  "https://twitter.com/MansterpieceAI/status/1716861609996419121?t=l9dVilSSu0JiAYo8AVtPmA&s=19",
+  "https://twitter.com/MansterpieceAI/status/1716795128537297115?t=pCmTH1InIniqTlHpY8cWQg&s=19",
+  "https://twitter.com/MansterpieceAI/status/1716674627982831713?t=nerNDcSDUcphFICLEiDYNQ&s=19",
+  "https://twitter.com/MansterpieceAI/status/1716529983638618265?t=CExSAZYJuW6BJoumbN6pJw&s=19",
+  "https://twitter.com/MansterpieceAI/status/1716529983638618265?t=0kzO9gOMR-qYLk7vcWPR_A&s=19",
+  "https://twitter.com/MansterpieceAI/status/1715777105982554256?t=2ra8kWRIOdMLixBnFSRACA&s=19",
+  "https://twitter.com/MansterpieceAI/status/1715520455673516065?t=wozRCmi25AEK1Uu6_eOh5A&s=19",
+  "https://twitter.com/MansterpieceAI/status/1714869735060394057?t=lc9gIliQYrrSSUvHkee-OA&s=19",
+  "https://twitter.com/WarriorErico/status/1714379934255218741?t=BzeSpV5_UhJfrEIC609VXA&s=19",
+  "https://twitter.com/MansterpieceAI/status/1714353444226576664?t=2YPzwN1QFFBzBHge0Zto6g&s=19",
+  "https://twitter.com/MansterpieceAI/status/1714231278000832528?t=JAY1SnFVhsJAwfsGvJgIpQ&s=19",
+  "https://twitter.com/MansterpieceAI/status/1714139674338632030?t=DXTlv2TM5vIW1vm8jcXxtA&s=19",
+  "https://twitter.com/MansterpieceAI/status/1713984825109958891?t=oef_faEpdG8rGspm4hFV2g&s=19",
+  "https://twitter.com/MansterpieceAI/status/1713886002954252305?t=N9a3DxUUrDGjpGT7G2hVoA&s=19",
+  "https://twitter.com/MansterpieceAI/status/1713465230485827903?t=6Jts1TX6bugOSKA-8-OOwA&s=19",
+  "https://twitter.com/MansterpieceAI/status/1713399295884562503?t=OPX4C1LgN5jYdCeP7R0pEg&s=19",
+  "https://twitter.com/AiFantacy/status/1713152941383983412?t=v1ll86JIx9ZwPc8JITNw4g&s=19",
+  "https://twitter.com/MansterpieceAI/status/1712170343127757276?t=sC4xTo4lRpRHapeIe9zxeQ&s=19",
+  "https://twitter.com/MansterpieceAI/status/1711747975687360834?t=WFT9RlKl0U1krHeargotiw&s=19",
+  "https://twitter.com/MansterpieceAI/status/1711705132528730203?t=oXZUujSjVnVgDY9DwpEnkA&s=19",
+  "https://twitter.com/MansterpieceAI/status/1711614283946094879?t=PUnDYVmG9-4Jox7a6dGOdQ&s=19",
+  "https://twitter.com/TheMansterpiece/status/1711416693296079159?t=9lnBWkk7y_Z7nkRO9MMsow&s=19",
+  "https://twitter.com/TheMansterpiece/status/1711283101807325421?t=i13JyaGX3V74gKNA6KEZJQ&s=19",
+  "https://twitter.com/TheMansterpiece/status/1711205989230256307?t=R1fh0wLPiJEY0Xw6xhS3ow&s=19",
+  "https://twitter.com/TheMansterpiece/status/1711129311351070823?t=yWROrVDJOhaIsjYNDj6QYw&s=19",
+  "https://twitter.com/TheMansterpiece/status/1711068314431050026?t=6RfzRdCYxCVsf2TBuR1hEg&s=19",
+  "https://twitter.com/TheMansterpiece/status/1710734645761884365?t=Pmv1-Z3DghRvoXcYnSRXSQ&s=19",
+  "https://twitter.com/FantacyAi15297/status/1710609653057671605?t=CL-E7lE3cPIDKhkak2f_ww&s=19",
+  "https://twitter.com/MansterpieceAI/status/1710642793931059690?t=HXWpWAduu3V8K2ED-oDTUg&s=19",
+  "https://twitter.com/TheMansterpiece/status/1709974966362423465?t=xkHhjb27G8hdJif5Mn-4Gw&s=19",
+  "https://twitter.com/TheMansterpiece/status/1709908678122353055?t=CNt2SBgwNGX9X2oEdGAJYg&s=19",
+  "https://twitter.com/ericowarrior/status/1709541199416049720?t=dqfkvvxUuEh_U1YjcshsGg&s=19",
+  "https://twitter.com/MansterpieceAI/status/1709657088857628826?t=x5duLjkfp8lyt9bYKqic0w&s=19",
+  "https://twitter.com/MansterpieceAI/status/1705210542363730225?t=7sWQOFRJiqOMaSxxIR1lFg&s=19",
+  "https://twitter.com/MansterpieceAI/status/1704625482887557130?t=uWONwlPnLBCF7x72ySPF4Q&s=19",
+  "https://twitter.com/MansterpieceAI/status/1704508210927825309?t=6Mwue6czTVLdNhEk2aoDLA&s=19",
+  "https://twitter.com/MansterpieceAI/status/1704502006042312753?t=YCGbQnAHy_KuuRqoS9DLvQ&s=19",
+  "https://twitter.com/MansterpieceAI/status/1704226604803862607?t=ujlIiVLy5fdXv8q04ye61Q&s=19",
+  "https://twitter.com/MansterpieceAI/status/1704044907428507698?t=IiqLpXn0fXImeXQgJxr22Q&s=19",
+  "https://twitter.com/MansterpieceAI/status/1703954272751071608?t=Wn9gAsmXNPrs7qsQAU50Ng&s=19",
+  "https://twitter.com/MansterpieceAI/status/1703135568635269198?t=aHrQanM2_XdMWPEfRu5DAQ&s=19",
+  "https://twitter.com/MansterpieceAI/status/1702334249897246941?t=_Zrzdyt_f3YiE9-TKvvu3Q&s=19",
+  "https://twitter.com/MansterpieceAI/status/1700959829051977956?t=K7cVPiY1ff1SNJyX57nbJg&s=19",
+  "https://twitter.com/MansterpieceAI/status/1705651294923341995?t=7eSy3N9qVUeao77vPelvTA&s=19",
+  "https://twitter.com/MansterpieceAI/status/1699842564638056580?t=DlsBIVzVLluCBbcWDwhyPg&s=19",
+  "https://twitter.com/MansterpieceAI/status/1700774356878848225?t=_W3FHSti1rU4VguBppmSsw&s=19",
+  "https://twitter.com/Sly920Sly/status/1705000255186407675?t=i9B5UdxC2Td2bx_JCR-zLw&s=19",
+  "https://twitter.com/Sly920Sly/status/1703984841027445247?t=N2gey4mEj96btjofq8b4hw&s=19",
+  "https://twitter.com/Sly920Sly/status/1703872965606035799?t=gY9fRKYL6jcsoO-psTYfFw&s=19",
+  "https://twitter.com/FantacyAi15297/status/1708361527680774448?t=hYkWUKz_NIEPOLZ-EoLhIQ&s=19",
+  "https://twitter.com/AiFantacy/status/1705013897160462441?t=pvAeTLvMov_CkOMRSTqJ2Q&s=19",
+  "https://twitter.com/AiFantacyPG/status/1703906190533788147?t=SQeTVzprsjiktvOITDBr3g&s=19",
+  "https://twitter.com/AiFantacyPG/status/1702089584597950828?t=1Ptca1wRTvHYYZiKHO1AQw&s=19",
+  "https://twitter.com/AiFantacyPG/status/1701731666430247001?t=h8dKBR94b2QcCbC00T7hqg&s=19",
+  "https://twitter.com/AiFantacyPG/status/1701574538331574608?t=0-AeC4FOaE5LVMpdPuleRA&s=19",
+  "https://twitter.com/AiFantacyPG/status/1701022962969985397?t=sq2JmRegJ5vk12TVZUqLzg&s=19",
+  "https://twitter.com/AiFantacyPG/status/1695222699038326996?t=aZXNDO03raCCKx7l0Iokpg&s=19",
+  "https://twitter.com/FantacyAi15297/status/1713553790341923298?t=Hc8aduMO-7lOMuSMZKUoLQ&s=19",
+  "https://twitter.com/AiFantacyPG/status/1706640272535544078?t=jCPOfuYLL25ShuoqDs7quA&s=19",
+  "https://twitter.com/AiFantacyPG/status/1706458480289464502?t=pYo1qf2gBClnVYzDj5winQ&s=19",
+  "https://twitter.com/AiFantacyPG/status/1702842579430359503?t=k49FfOeZrVUzctqEnK3Dkw&s=19",
+];
+global.videosxxxc = [
+  'https://telegra.ph/file/4a270d9945ac46f42d95c.mp4',
+  'https://telegra.ph/file/958c11e84d271e783ea3f.mp4',
+  'https://telegra.ph/file/f753759342337c4012b3f.mp4',
+  'https://telegra.ph/file/379cee56c908dd536dd33.mp4',
+  'https://telegra.ph/file/411d8f59a5cefc2a1d227.mp4',
+  'https://telegra.ph/file/ee2cf1b359d6eef50d7b7.mp4',
+  'https://telegra.ph/file/1e316b25c787f94a0f8fd.mp4',
+  'https://telegra.ph/file/c229d33edce798cde0ca4.mp4',
+  'https://telegra.ph/file/b44223e72dd7e80e415f2.mp4',
+  'https://telegra.ph/file/61486d45a8a3ea95a7c86.mp4',
+  'https://telegra.ph/file/76ba0dc2a07f491756377.mp4',
+  'https://telegra.ph/file/831bb88f562bef3f1a15d.mp4',
+  'https://telegra.ph/file/ee2cf1b359d6eef50d7b7.mp4',
+  'https://telegra.ph/file/598857924f3a29ffd37ae.mp4',
+  'https://telegra.ph/file/528caef6ea950ec45aeef.mp4',
+  'https://telegra.ph/file/4a270d9945ac46f42d95c.mp4',
+  'https://telegra.ph/file/958c11e84d271e783ea3f.mp4',
+  'https://telegra.ph/file/f753759342337c4012b3f.mp4',
+  'https://telegra.ph/file/379cee56c908dd536dd33.mp4',
+  'https://telegra.ph/file/411d8f59a5cefc2a1d227.mp4',
+  'https://telegra.ph/file/76ba0dc2a07f491756377.mp4',
+  'https://telegra.ph/file/831bb88f562bef3f1a15d.mp4',
+];
+
+global.videosxxxc2 = [
+"https://telegra.ph/file/2dfb1ad0cab22951e30d1.mp4",
+"https://telegra.ph/file/c430651857023968d3a76.mp4",
+"https://telegra.ph/file/1ba17f6230dd1ea2de48c.mp4",
+"https://telegra.ph/file/e04b802f12aafee3d314e.mp4",
+"https://telegra.ph/file/a58661697d519d3d0acbd.mp4",
+"https://telegra.ph/file/9ed60b18e79fcfebcd76c.mp4",
+"https://telegra.ph/file/d58096000ad5eaef0b05e.mp4",
+"https://telegra.ph/file/60b4c8ebeadebb7e0da06.mp4"
+];
