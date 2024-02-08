01adea5c662e1ed988353c50837e8956013b8fbd
Support-Bots-OFC <155838423+Bots-WhatsApp-OFC@users.noreply.github.com>
2024-02-08 14:30:12 -0300

    Update info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -1,14 +1,14 @@
 import fs from 'fs';
 const handler = (m) => m;
 handler.all = async function(m) {
-  const vn = './media/Onichan.mp3';
+  const vn = './media/bot.mp3';
   const chat = global.db.data.chats[m.chat];
   //const s = seconds: '4556'
   // const estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: {orderMessage: { itemCount : -999999, status: 1, surface : 1, message: '𝑆𝑎𝑘𝑢𝑟𝑎 - 𝐵𝑜𝑡', orderTitle: 'Bang', thumbnail: fs.readFileSync('./Menu2.jpg'), sellerJid: '0@s.whatsapp.net'}}}
   // const estiloaudio = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: {"audioMessage": { "mimetype":"audio/ogg; codecs=opus", "seconds": "99569", "ptt": "true"}}}
   if (/^bot$/i.test(m.text) && !chat.isBanned) {
     conn.sendPresenceUpdate('recording', m.chat);
-    await m.reply(`*😄 𝙃𝙤𝙡𝙖𝙖, 𝙚𝙣 𝙦𝙪𝙚 𝙩𝙚 𝙥𝙪𝙚𝙙𝙤 𝙖𝙩𝙮𝙙𝙖𝙧?*`);
+    await m.reply(`*😄 Hola usuario/a, ¿en que te puedo ayudar?*`);
     m.conn.sendMessage(m.chat, {audio: {url: vn}, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
     // conn.sendFile(m.chat, vn, 'bot.mp3', null, m, true, { type: 'audioMessage', seconds: '4556', ptt: true, sendEphemeral: true, quoted: m })
   }
