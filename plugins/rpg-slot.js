14fedf9b59fcde34873457888efad729cf329352
sinombre913 <145749930+sinombre913@users.noreply.github.com>
2024-03-14 16:51:07 -0600

    Update rpg-slot.js

--- plugins/rpg-slot.js
+++ plugins/rpg-slot.js
@@ -5,7 +5,7 @@ const handler = async (m, {conn, isPrems}) => {
   const hasil = Math.floor(Math.random() * 5000);
   const time = global.db.data.users[m.sender].lastslut + 60000;
   if (new Date - global.db.data.users[m.sender].lastslut < 60000) throw `*¡Espera un momento pequeño!* \n\n*—◉ Regresa en ${msToTime(time - new Date())} ⏳*`;
-  conn.sendMessage(m.chat, {text: `*${pickRandom(global.slut)}*\n\n*!Te Pagan Con ${hasil} exp!*`}, {quoted: m});
+  conn.sendMessage(m.chat, {text: `*${pickRandom(global.slut)}*\n\n*!Te Paga(n) Con ${hasil} exp!*`}, {quoted: m});
   global.db.data.users[m.sender].exp += hasil;
   global.db.data.users[m.sender].lastslut = new Date() * 1;
   enviando = false
@@ -32,7 +32,7 @@ function pickRandom(list) {
 }
 
 global.slut = [
-  'Le Chupas La Verga Al Creador De NigromanteBot-MD.',
+  'Se La Chupas A La Staff De StivenBot-MD.',
   'Te Combiertes en la zorra de un millonario.',
   'Dejas Que Un Ramdom Del Grupo Te Coja.',
   'Exploras Una Montaña Y Un Campesino Te Coje.',
